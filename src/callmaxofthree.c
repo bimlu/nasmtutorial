abbd63ab0e1548087776c788e8a25ee1f566e73d
Robin Rawat <robinrawat7017@gmail.com>
2021-07-28 18:14:38 +0530

    mixing c and assembly language

---
+++ src/callmaxofthree.c
@@ -0,0 +1,16 @@
+/*
+ * A small program that illustrates how to call the maxofthree function we wrote in
+ * assembly language.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+
+int64_t maxofthree(int64_t, int64_t, int64_t);
+
+int main() {
+    printf("%ld\n", maxofthree(1, -4, -7));
+    printf("%ld\n", maxofthree(2, -6, 1));
+    printf("%ld\n", maxofthree(2, 3, 1));
+    return 0;
+}
